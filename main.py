e4fc5b8a3889bc363cc2518fbbf65b727e8371ce
nils-n <nilsfuruskog@yandex.ru>
2020-05-16 23:08:07 +0300

    tets commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+from MyRPG.Constants import *
+from MyRPG.player import *
+
+
+class Main:
+    def __init__(self, screen_0):
+        self.screen = screen_0
+        self.player = Player('Nils')
+        self.background = pygame.image.load("png/background_2.jpg")
+        self.running = True
+        self.main_loop()
+
+    def handle_events(self):
+        pass
+
+    def render(self):
+        # прорисовка всего-всего
+
+        self.screen.blit(self.background, (0, 0))
+        self.player.render(screen)
+        pygame.display.flip()
+
+    def main_loop(self):
+        # основной цыкл программы
+
+        while self.running is True:
+            self.render()
+
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+Main(screen)
---
+++ player.py
@@ -0,0 +1,43 @@
+import pygame
+from MyRPG.Constants import *
+
+
+def render(screen):
+    screen.blit()
+
+    pass
+
+
+class Player:
+    def __init__(self, name):
+        self.state = ALIVE
+        self.direction = RIGHT
+        self.x = START_X
+        self.y = START_Y
+        self.name = name
+        self.hp = MAX_HP
+        self.mp = MAX_MP
+        self.image_pack = ['kosmanaft.2.0.jpg']
+        self.images = []
+        for image in self.image_pack:
+            temp = pygame.image.load(image).convert_alpha()
+            i = []
+            i.append(temp.subsurface(0, 0, 196, 196))
+            i.append(temp.subsurface(196, 0, 196, 196))
+            i.append(temp.subsurface(392, 0, 196, 196))
+            i.append(temp.subsurface(588, 0, 196, 196))
+            i.append(temp.subsurface(784, 0, 196, 196))
+            self.images.append(i)
+
+        self.moving = [0, 0, 0, 0]
+
+    def move(self):
+        pass
+
+    def render(self, screen):
+        screen.blit(self.images[self.direction][self.state], (self.x, self.y))
+
+    def render_ui(self, screen):
+
+        pass
+
